09f4ef2daad047d59ff26aeff2711ec530adfc00
ogabriel <gabrieloliver8991@gmail.com>
2023-10-15 14:57:00 -0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-cd /rails
-sleep 5 # just to make sure postgres is up
-echo "run db:reset"
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:reset db:create db:migrate
-echo "run Puma"
-bin/bundle exec puma -C config/puma.rb
+
+if [ "$1" = 'dev' ]; then
+    cd /rails
+    bundle install
+    exec "$2"
+elif [ "$1" = 'server' ]; then
+    cd /rails
+    exec /rails/bin/rails server
+elif [ "$1" = 'migrate_and_server' ]; then
+    cd /rails
+    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 /rails/bin/rails db:drop db:create db:migrate
+    exec /rails/bin/rails server
+elif [ "$1" = 'sidekiq' ]; then
+    cd /rails
+    exec /rails/bin/bundle exec sidekiq -c 1 -q flush
+fi
